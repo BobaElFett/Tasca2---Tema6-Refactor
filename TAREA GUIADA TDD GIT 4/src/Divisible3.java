533c9afcbff9757b768923ee6d7e4e457bb1bd02
Ferran <fmontblanch@gmail.com>
2023-05-05 13:04:18 +0200

    Sin parse int

--- TAREA GUIADA TDD GIT 4/src/Divisible3.java
+++ TAREA GUIADA TDD GIT 4/src/Divisible3.java
@@ -1,6 +1,15 @@
 public class Divisible3 {
 
-   public static boolean divisible(long num){
-        return num % 3 == 0;
+   public static boolean divisible(int n){
+
+       long num;
+
+       String numSol = "";
+
+       for (int i = 1; i <= n; i++) {
+           numSol += i;
+       }
+
+        return Long.parseLong(numSol) % 3 == 0;
     }
 }
